a315a9730407dc9626d1868d2a85336fe49c298b
Andreas <a.hacker1593@gmail.com>
2026-06-01 15:41:39 +0200

    build: break the homepage

---
+++ README.md
@@ -0,0 +1,5 @@
+Gehostet bei Vercel
+
+Domain bei IONOS
+User: 5129691
+PW: s. Unterlagen / nachfragen
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/layouts/main'
-import Fonts from '../components/fonts'
-import theme from '../lib/theme'
-import { AnimatePresence } from 'framer-motion'
-
-const Website = ({ Component, pageProps, router }) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Layout router={router}>
-                <AnimatePresence exitBeforeEnter initial={true}>
-                    <Component {...pageProps} key={router.route} />
-                </AnimatePresence>
-            </Layout>
-        </ChakraProvider>
-    )
+export default function Website() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '20px'
+      }}
+    >
+      Nothing found here
+    </div>
+  )
 }
-
-export default Website
